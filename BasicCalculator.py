14e4f6397f0e6d2a4e8e60fe0034c0bc9c639063
Andrew Bass <48231637+bassandrew35@users.noreply.github.com>
2019-03-29 18:28:37 -0500

    Version 1.1

--- BasicCalculator.py
+++ BasicCalculator.py
@@ -3,58 +3,60 @@
 def addition():
     print("Enter first number")
     x = input()
-    num1 = int(x)
+    num1 = float(x)
     print("Enter second number")
     y = input()
-    num2 = int(y)
+    num2 = float(y)
     return (num1 + num2)
 
 def subtraction():
-    print("Enter first number")
+    print("Enter number to be subtracted")
     x = input()
-    num1 = int(x)
+    num1 = float(x)
     print("Enter second number")
     y = input()
-    num2 = int(y)
+    num2 = float(y)
     return (num1 - num2)
 
 def multiplication():
     print("Enter first number")
     x = input()
-    num1 = int(x)
+    num1 = float(x)
     print("Enter second number")
     y = input()
-    num2 = int(y)
+    num2 = float(y)
     return (num1 * num2)
 
 def division():
-    print("Enter first number")
+    print("Enter dividend")
     x = input()
-    num1 = int(x)
-    print("Enter second number")
+    num1 = float(x)
+    print("Enter divisor")
     y = input()
-    num2 = int(y)
+    num2 = float(y)
     return (num1 / num2)
 
+optionChoice = ["Addition", "Subtraction", "Multiplication","Division"]
 for x in range(2):
     print("Enter number of choice")
     print("1: Add")
     print("2: Subtract")
     print("3: Multiply")
     print("4: Divide")
-    option = input();
-    print(option)
-    if option == "1":
+    optionNum = input();
+    optionNum = int(optionNum)
+    print(optionChoice[optionNum - 1], ":")
+    if optionNum == 1:
         result = addition()
-        print(result)
-    elif option == "2":
+        print("Answer ---> ", result)
+    elif optionNum == 2:
         result = subtraction()
-        print(result)
-    elif option == "3":
+        print("Answer ---> ", result)
+    elif optionNum == 3:
         result = multiplication()
-        print(result)
-    elif option == "4":
+        print("Answer ---> ", result)
+    elif optionNum == 4:
         result = division()
-        print(result)
+        print("Answer ---> ", result)
     else:
         print("invalid option")
